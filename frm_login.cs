6d9653c5ac66ce27c10ff635f83e22a0aacc7a69
Abz Amerol <amerolabdulsalam@gmail.com>
2025-03-07 10:21:35 +0800

    Added Connection.cs, frm_ammunganhall.Designer.cs and frm_login.cs files

---
+++ frm_login.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Net.Sockets;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace pgso
+{
+    public partial class frm_login: Form
+    {
+        Connection con = new Connection();
+
+        SqlCommand cmd;
+        string strSQL;
+        SqlDataAdapter da;
+        DataTable dt;
+        public frm_login()
+        {
+            InitializeComponent();
+        }
+
+        private void frm_login_Load(object sender, EventArgs e)
+        {
+            // SKIPS LOGIN in DEBUG MODE / Auto-login when running inside Visual Studio Debug Mode
+            if (Debugger.IsAttached)
+            {
+                this.Hide();
+                frm_dashboard dashboard = new frm_dashboard();
+                dashboard.ShowDialog();
+                this.Close();
+            }
+        }
+        //improve ito soon.Pansamantala muna to. basta makalogin muna
+        private void button1_Click(object sender, EventArgs e)
+        {
+          
+
+            strSQL = "SELECT * FROM users WHERE name='" + combouname.Text + "' AND password='" + txtpassword.Text + "'";
+
+            cmd = new SqlCommand(strSQL, con.strCon);
+            cmd.CommandTimeout = 360;
+            da = new SqlDataAdapter(cmd);
+            dt = new DataTable();
+            da.Fill(dt);
+
+           if (dt.Rows.Count != 0)
+
+            {
+                DataRow row = dt.Rows[0];
+                this.Hide();
+
+                frm_dashboard frm = new frm_dashboard();
+                frm.ShowDialog();
+
+            }
+
+
+            else
+            {
+                MessageBox.Show("Invalid username or password");
+
+            }
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+       
+
+        
+
+        private void lbl_register_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            frm_register frm = new frm_register();
+            frm.ShowDialog();
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void lbl_dateandtime_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dateTimePicker2_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dateTimePicker1_ValueChanged_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtpassword_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btneye_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
